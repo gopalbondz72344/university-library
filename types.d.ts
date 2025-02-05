f63faf2f4ece818cf1ab6f087e2c7e39a6748171
saigopal-dev <saigopal.mepstra@gmail.com>
2025-02-05 11:58:36 +0530

    update bookwise

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,5 @@
 interface Book {
-  id: number;
+  id: string;
   title: string;
   author: string;
   genre: string;
@@ -11,6 +11,7 @@ interface Book {
   coverColor: string;
   videoUrl: string;
   summary: string;
+  createdAt: Date | null;
   isLoanedBook?: boolean;
 }
 
@@ -20,4 +21,22 @@ interface AuthCredentials {
   password: string;
   universityId: number;
   universityCard: string;
+}
+
+interface BookParams {
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  coverUrl: string;
+  coverColor: string;
+  description: string;
+  totalCopies: number;
+  videoUrl: string;
+  summary: string;
+}
+
+interface BorrowBookParams {
+  bookId: string;
+  userId: string;
 }
